e63b6024d3e179f8994e433ca0bc30f51f06068e
Chelsea Griffo <chelseagriffo@gmail.com>
2023-02-25 13:56:26 -0600

    Added menu and error message

--- app.py
+++ app.py
@@ -1,6 +1,27 @@
 from models import (Base, session, 
                     Book, engine)
 
+
+def menu():
+    while True:
+        print('''
+            \nPROGRAMMING BOOKS
+            \r1) Add book
+            \r2) View all books
+            \r3) Search for book
+            \r4) Book Analysis
+            \r5) Exit
+            ''')
+        choice = input('What would you like to do? ')
+        if choice in ['1', '2', '3', '4', '5']:
+            return choice
+        else:
+            input('''
+                \rPlease choose one of the options above. 
+                \rA number from 1-5.
+                \rPress enter to try again.
+                ''')
+
 #import models
 #main menu - add, search, analysis, exit, view
 #add books to the database
@@ -12,3 +33,4 @@ from models import (Base, session,
 
 if __name__ == '__main__':
     Base.metadata.create_all(engine)
+    menu()
